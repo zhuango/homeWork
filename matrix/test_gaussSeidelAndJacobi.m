b58cd88da12b28dc69ccd6acb91de6217b7148e9
Og192 <og192liu@gmail.com>
2016-12-02 17:48:25 +0800

    cal new

--- matrix/test_gaussSeidelAndJacobi.m
+++ matrix/test_gaussSeidelAndJacobi.m
@@ -1,15 +1,23 @@
 %A = reshape([8, 4, 2, -3, 11, 1, 2, -1, 4], 3, 3);
 %b = [20, 33, 12]';
 
-%x = gaussSeidel(A, b, [0, 0, 0]', 10)
-%x = jacobi(A, b, [0, 0, 0]', 10)
+%x = gaussSeidel(A, b, [0, 0, 0]', 1e-3)
+%x = jacobi(A, b, [0, 0, 0]', 1e-3)
 
 A = reshape([2, 1, 1, -1, 1, 1, 1, 1, -2], 3, 3);
 b = [1, 1, 1]';
-
-gaussSeidel_x = gaussSeidel(A, b, [0, 0, 0]', 10)
+gaussSeidel_x = gaussSeidel(A, b, [0, 0, 0]', 1e-3)
 
 A = reshape([1, -1, -2, -2, 1, -2, 2, -1, 1], 3, 3);
 b = [1, 1, 1]';
+jacobi_x = jacobi(A, b, [0, 0, 0]', 1e-3)
 
-jacobi_x = jacobi(A, b, [0, 0, 0]', 10)
+
+A = reshape([5, -1, -3, -1, 2, 4, -3, 4, 15], 3, 3);
+b = [-2, 1, 10]';
+gaussSeidel_x = gaussSeidel(A, b, [0, 0, 0]', 1e-3)
+
+
+A = reshape([5, -1, -3, -1, 2, 4, -3, 4, 15], 3, 3);
+b = [-2, 1, 10]';
+jacobi_x = jacobi(A, b, [0, 0, 0]', 1e-3, 1000)

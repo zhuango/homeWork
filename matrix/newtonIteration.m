8471c226f6ed16796e906f3ef632154f9e15706c
Og192 <og192liu@gmail.com>
2016-12-02 17:48:07 +0800

    add newtion lambda

--- matrix/newtonIteration.m
+++ matrix/newtonIteration.m
@@ -1,11 +1,11 @@
-function [x] = newtonIteration(initialValue, precision)
+function [x] = newtonIteration(initialValue, precision, lambda = 1.0)
     old = initialValue;
-    x   = old - newtonIteration_f(old) / newtonIteration_derivative(old);
+    x   = old - lambda * newtonIteration_f(old) / newtonIteration_derivative(old);
     format;old, x
     format;error = abs(x - old)
     while abs(x - old) > precision
         old = x;
-        x   = old - newtonIteration_f(old) / newtonIteration_derivative(old);
+        x   = old - lambda * newtonIteration_f(old) / newtonIteration_derivative(old);
         format;old, x
         format;error = abs(x - old)
     end 

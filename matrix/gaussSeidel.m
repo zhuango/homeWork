f2c1b7039eca5309cbd9612d4c0b6e7a2ffa1720
Og192 <og192liu@gmail.com>
2016-12-02 17:47:54 +0800

    add newton precision

--- matrix/gaussSeidel.m
+++ matrix/gaussSeidel.m
@@ -1,14 +1,22 @@
-function [x] = gaussSeidel(A, b, initialValues, steps)
+function [x] = gaussSeidel(A, b, initialValues, precision, maxSteps = 100)
     order = size(A, 1);
-    D = eye(order) .* A
+    D = eye(order) .* A;
     L = -tril(A, -1);
     U = -triu(A,  1);
 
     BG = (D - L) ^ -1 * U;
     f  = (D - L) ^ -1 * b;
 
-    x = initialValues;
-    for i = 1:steps
+    x    = initialValues;
+    oldx = x;
+    for i = 1:maxSteps
         x = BG * x + f;
+        
+        error = abs(x - oldx);
+        if max(error) < precision
+            break
+        end
+        
+        oldx = x;
     end
 end
--- matrix/jacobi.m
+++ matrix/jacobi.m
@@ -1,4 +1,4 @@
-function [x] = jacobi(A, b, initialValues, steps)
+function [x] = jacobi(A, b, initialValues, precision, maxSteps = 100)
     order = size(A, 1);
     D = eye(order) .* A;
     L = -tril(A, -1);
@@ -7,8 +7,16 @@ function [x] = jacobi(A, b, initialValues, steps)
     BJ = D ^ -1 * (L + U);
     f  = D ^ -1 * b;
 
-    x = initialValues;
-    for i = 1:steps
+    x    = initialValues;
+    oldx = x;
+    for i = 1:maxSteps
         x = BJ * x + f;
+
+        error = abs(x - oldx);
+        if max(error) < precision
+            break
+        end
+
+        oldx = x;
     end
 end

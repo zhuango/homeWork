cc52e67f1e1d49b671ff7f1f2107687c783609f8
Og192 <og192liu@gmail.com>
2016-12-08 11:42:07 +0800

    debug new interpolation

--- matrix/newtonInterpolation_pol.m
+++ matrix/newtonInterpolation_pol.m
@@ -1,10 +1,11 @@
 function [px]  = newtonInterpolation_pol(x, interpolations, polCoefficients)
-    n = size(polCoefficients);
-    px = 0;
+    n = max(size(polCoefficients));
+    px = 0.0;
     for i = 1:n
         temp = 1.0;
         for j = 2:i
             temp = temp .* (x - interpolations(j - 1));
+        end
         px += temp .* polCoefficients(i);
     end
 end

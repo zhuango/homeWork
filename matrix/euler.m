31208bdb1d3720e7572122d9df897b281811b34b
Og192 <og192liu@gmail.com>
2016-12-10 08:39:21 +0800

    euler

--- matrix/euler.m
+++ matrix/euler.m
@@ -1,4 +1,12 @@
-function [u] = euler(start, initialValue, stepLength, stepCount)
-    u = zeros(stepCount, 1)
-    
+function [u] = euler(startPoint, initialValue, endPoint, stepLength)
+    stepCount = (endPoint - startPoint) / stepLength;
+    u = zeros(stepCount + 1, 1);
+    u(1) = initialValue;
+
+    currentPoint = startPoint;
+
+    for i = 1:stepCount
+        u(i+1) = u(i) + stepLength .* euler_f(currentPoint, u(i));
+        currentPoint += stepLength;
+    end
 end
---
+++ matrix/euler_f.m
@@ -0,0 +1,5 @@
+function [yt] = euler_f(t, y)
+    yt = (t .* y - y .* 2) ./ ((t + 1) .^ 2);
+    %yt = 1 - (2 .* t .* y) / (1 + t.^2);
+
+end

18a9d563e25af5290de2b939656f01b55eae7838
Og192 <og192liu@gmail.com>
2016-12-12 16:43:47 +0800

    rename

--- matrix/item02/fx.m
+++ matrix/item02/fx.m
@@ -1,10 +1,11 @@
-function [result] = fx(x)
+function [result] = fxBad(x)
     result = zeros(size(x));
+    d = 1 + x;
     for i = 1:max(size(x))
-        if x(i) == 0.0
+        if d(i) == 1
             result(i) = 1.0;
-            continue;
+            continue
         end
-        result(i) = log(1.0 + x(i)) ./ x(i);
+        result(i) = log(d(i)) ./ (d(i) - 1);
     end
 end
--- matrix/item02/fxBad.m
+++ matrix/item02/fxBad.m
@@ -1,11 +1,10 @@
-function [result] = fxBad(x)
+function [result] = fx(x)
     result = zeros(size(x));
-    d = 1 + x;
     for i = 1:max(size(x))
-        if d(i) == 1
+        if x(i) == 0.0
             result(i) = 1.0;
-            continue
+            continue;
         end
-        result(i) = log(d(i)) ./ (d(i) - 1);
+        result(i) = log(1.0 + x(i)) ./ x(i);
     end
 end

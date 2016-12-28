6b3659f7eb88de23d792b12ec6b2869e05e861c9
Og192 <og192liu@gmail.com>
2016-12-28 11:26:01 +0800

    change commit position. at the end of all insertions then commit all changed

--- searchEngine/recommand/writeDataIntoMysql.py
+++ searchEngine/recommand/writeDataIntoMysql.py
@@ -1,6 +1,7 @@
-dataPath = "/home/laboratory/Downloads/dutir_tianchi_mobile_recommend_train_user_train.csv"
 import csv
 import MySQLdb
+
+dataPath = "/home/laboratory/Downloads/dutir_tianchi_mobile_recommend_train_user_train.csv"
 
 conn=MySQLdb.connect(host='localhost',user='root',passwd='lab',db='machineLearning',port=3306)
 cur=conn.cursor()
@@ -17,11 +18,11 @@ with open(dataPath, 'rb') as csvfile:
         try:
             sql = "INSERT INTO `dutir_tianchi_mobile_recommend_train_user_train`(`user_id`, `item_id`, `behavior_type`, `user_geohash`, `item_category`, `time`) VALUES ('"+value1+"','"+value2+"','"+value3+"','"+value4+"','"+value5+"','"+value6+":00:00')"
             cur.execute(sql)
-            conn.commit()
         except MySQLdb.Error,e:
             print "Mysql Error %d: %s" % (e.args[0], e.args[1])
             print(row)
 
+conn.commit()
 cur.close()
 conn.close()
 

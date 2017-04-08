58652925d11bf39da2030d4b213525a1b6c3735a
Og192 <og192liu@gmail.com>
2017-04-08 12:23:56 +0800

    add feature function

--- machineTranslation/crf.py
+++ machineTranslation/crf.py
@@ -2,21 +2,37 @@
 
 import numpy as np
 
-class Sample():
-    def __init__(self, data, seqLength, seqStates):
-        self.seqLength = seqLength
-        self.seqStates = seqStates
-    
-    # all 0-base.
-    def getFeature(self, seqNum, y1State, y2State = None):
-        pass
+class Sample(object):
+
+    def __init__(self, s, truth=None):
+        self.sequence = list(s)
+        self.truth = truth
+        self.Length = len(s)
+        # CRF will cache data here
+        self.feature_table = None
+        self.target_features = None
+
+    def GetFeature(self, seqNum, y1State, y2State):
+        """ Create the vector of active indicies for this label setting. """
+        # label-label-token; bigram features
+        #for f in self.sequence[t].attributes:
+        #    yield '[%s,%s,%s]' % (yp,y,f)
+        # label-token; emission features
+        if y2State:
+            # label-label; transition feature
+            yield '[%s,%s]' % (y1State,y2State)
+        else:
+            for f in self.sequence[t].attributes:
+                yield '[%s,%s]' % (y,f)
+            # label "prior" feature
+            yield '[%s]' % y
+
 
 class CRF:
-    def __init__(self, nodeFeatureSize, edgeFeatureSize, N, K):
-        self.mWnode = np.random.uniform(-1, 1, nodeFeatureSize)
-        self.mWedge = np.random.uniform(-1, 1, edgeFeatureSize)
-        self.mObservationStateSize = N
-        self.mLabelStateSize = K
+    def __init__(self, maxLength, nodeFeatureSize, edgeFeatureSize, labelStateSize):
+        self.mWnode = np.random.uniform(-1, 1, (maxLength, nodeFeatureSize))
+        self.mWedge = np.random.uniform(-1, 1, (maxLength - 1, edgeFeatureSize))
+        self.mLabelStateSize = labelStateSize
 
     # def LogPotentials(self, nodeFeatures, edgeFeatures):
     #     sumNodeFeatures = np.sum(nodeFeatures, 1)
@@ -34,12 +50,12 @@ class CRF:
         logPotentials = np.zeros((seqLength-1, self.mLabelStateSize, self.mLabelStateSize))
 
         for i in xrange(0, self.mLabelStateSize):
-            logPotential0[i] = np.dot(self.mWnode, getFeature(0, i))
+            logPotential0[i] = np.dot(self.mWnode[0], getFeature(0, i))
 
         for i in xrange(1, seqLength):
             for j in xrange(0, self.mLabelStateSize):
                 for k in xrange(0, self.mLabelStateSize):
-                    logPotentials[i-1][j][k] = np.dot(self.mNode, getFeature(i, j)) + np.dot(self.mWedge, getFeature(i, j, k))
+                    logPotentials[i-1][j][k] = np.dot(self.mWnode[i], getFeature(i, j)) + np.dot(self.mWedge[i-1], getFeature(i, j, k))
         
         return (logPotential0, logPotentials)
     
@@ -85,12 +101,12 @@ class CRF:
 
         for i in xrange(0, seqLength):
             for j in xrange(0, self.mLabelStateSize):
-                WnodeGradient += numpy.exp(forwardMessages[i] + backwardMessages[i] - logNormalized) * getFeature(i, j)
+                WnodeGradient[i] += np.exp(forwardMessages[i] + backwardMessages[i] - logNormalized) * getFeature(i, j)
         
         for i in xrange(1, seqLength):
             for j in xrange(0, self.mLabelStateSize):
                 for k in xrange(0, self.mLabelStateSize):
-                    WnodeGradient += numpy.exp(forwardMessages[i]+ logPotentials[i] + backwardMessages[i] - logNormalized) * getFeature(i, j, k)
+                    WnodeGradient[i-1] += np.exp(forwardMessages[i]+ logPotentials[i] + backwardMessages[i] - logNormalized) * getFeature(i, j, k)
 
         return (WnodeGradient, WedgeGradient)
 
@@ -104,7 +120,7 @@ class CRF:
         pathMatrix = np.zeros((seqLength, self.mLabelStateSize))
         w = logPotential0
         for i in xrange(1, seqLength):
-            w = logPotentials[i]+ numpy.reshape(w, (3, 1))
+            w = logPotentials[i]+ np.reshape(w, (3, 1))
             pathMatrix[i] = w.argmax(0)
             w = w.max(0)
         labels[-1] = w.argmax()
@@ -124,9 +140,9 @@ class CRF:
 
             for j in xrange(0, sequence.Length):
                 for k in xrange(0, self.mLabelStateSize):
-                    self.mWnode += sequence.GetFeature(j, k) * rate
+                    self.mWnode[j] += getFeature(j, k) * rate
 
             for j in xrange(1, sequence.Length):
                 for k in srange(0, self.mLabelStateSize):
                     for l in xrange(0, self.mLabelStateSize):
-                        self.mWedge += sequence.GetFeature(j, k, l) * rate
+                        self.mWedge[j-1] += getFeature(j, k, l) * rate

f03ab8f4a694c79001f15eee483e902cd06cdcfb
Og192 <og192liu@gmail.com>
2017-04-09 11:53:22 +0800

    get feature

--- machineTranslation/CRFModel/crf.py
+++ machineTranslation/CRFModel/crf.py
@@ -3,14 +3,12 @@
 import numpy as np
 
 class Sample(object):
-
-    def __init__(self, s, truth=None):
+    LabelTable = {}
+    WordsTable = {}
+    def __init__(self, s, labels=None):
         self.sequence = list(s)
-        self.truth = truth
+        self.Labels = labels
         self.Length = len(s)
-        # CRF will cache data here
-        self.feature_table = None
-        self.target_features = None
 
     def GetFeature(self, seqNum, y1State, y2State):
         """ Create the vector of active indicies for this label setting. """
@@ -18,20 +16,25 @@ class Sample(object):
         #for f in self.sequence[t].attributes:
         #    yield '[%s,%s,%s]' % (yp,y,f)
         # label-token; emission features
-        if y2State:
-            # label-label; transition feature
-            yield '[%s,%s]' % (y1State,y2State)
-        else:
-            for f in self.sequence[t].attributes:
-                yield '[%s,%s]' % (y,f)
-            # label "prior" feature
-            yield '[%s]' % y
+        word0  = self.sequence[seqNum]
+        label0 = self.LabelTable[y1State]
 
+        if y2State:
+            word1  = self.sequence[seqNum-1]
+            label1 = self.LabelTable[y2State]
+            f0 = float(if label0 == 'a' and label1 == 'n')
+            f1 = float(if label0 == 'p' and label1 == 'p')
+            feature = np.append(WordsTable[word0] - WordsTable[word1], [f0, f1])
+            return feature
+        else:
+            feature = WordsTable[word0]
+            return feature
+            
 
 class CRF:
-    def __init__(self, maxLength, nodeFeatureSize, edgeFeatureSize, labelStateSize):
-        self.mWnode = np.random.uniform(-1, 1, (maxLength, nodeFeatureSize))
-        self.mWedge = np.random.uniform(-1, 1, (maxLength - 1, edgeFeatureSize))
+    def __init__(self, nodeFeatureSize, edgeFeatureSize, labelStateSize):
+        self.mWnode = np.random.uniform(-1, 1, nodeFeatureSize)
+        self.mWedge = np.random.uniform(-1, 1, edgeFeatureSize)
         self.mLabelStateSize = labelStateSize
 
     # def LogPotentials(self, nodeFeatures, edgeFeatures):
@@ -50,12 +53,12 @@ class CRF:
         logPotentials = np.zeros((seqLength-1, self.mLabelStateSize, self.mLabelStateSize))
 
         for i in xrange(0, self.mLabelStateSize):
-            logPotential0[i] = np.dot(self.mWnode[0], getFeature(0, i))
+            logPotential0[i] = np.dot(self.mWnode, getFeature(0, i))
 
         for i in xrange(1, seqLength):
             for j in xrange(0, self.mLabelStateSize):
                 for k in xrange(0, self.mLabelStateSize):
-                    logPotentials[i-1][j][k] = np.dot(self.mWnode[i], getFeature(i, j)) + np.dot(self.mWedge[i-1], getFeature(i, j, k))
+                    logPotentials[i-1][j][k] = np.dot(self.mWnode, getFeature(i, j)) + np.dot(self.mWedge, getFeature(i, j, k))
         
         return (logPotential0, logPotentials)
     
@@ -106,7 +109,7 @@ class CRF:
         for i in xrange(1, seqLength):
             for j in xrange(0, self.mLabelStateSize):
                 for k in xrange(0, self.mLabelStateSize):
-                    WnodeGradient[i-1] += np.exp(forwardMessages[i]+ logPotentials[i] + backwardMessages[i] - logNormalized) * getFeature(i, j, k)
+                    WnodeGradient[i-1] += np.exp(forwardMessages[i] + logPotentials[i] + backwardMessages[i] - logNormalized) * getFeature(i, j, k)
 
         return (WnodeGradient, WedgeGradient)
 
@@ -140,9 +143,9 @@ class CRF:
 
             for j in xrange(0, sequence.Length):
                 for k in xrange(0, self.mLabelStateSize):
-                    self.mWnode[j] += getFeature(j, k) * rate
+                    self.mWnode += getFeature(j, k) * rate
 
             for j in xrange(1, sequence.Length):
                 for k in srange(0, self.mLabelStateSize):
                     for l in xrange(0, self.mLabelStateSize):
-                        self.mWedge[j-1] += getFeature(j, k, l) * rate
+                        self.mWedge += getFeature(j, k, l) * rate

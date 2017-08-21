0b661be5bfe4409165478b808d8fa3fea7b2e485
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2017-08-21 15:35:12 +0800

    recode and generalize

--- machineTranslation/preprocess/recode.py
+++ machineTranslation/preprocess/recode.py
@@ -1,15 +1,18 @@
-filename = "../data/199801.txt"
-recodeFilename = "../data/199801_utf8.txt"
+#!/usr/bin/python2
+import re
+filename = "../data/2000-09-2003.txt.utf8"
+recodeFilename = "../data/2000-09-2003.txt.utf8.recode"
 
 newFileStream = open(recodeFilename, 'w')
 
 with open(filename, 'r') as f:
     for line in f:
-        lineDecoded = line.decode('gbk')
+        lineDecoded = line.decode('utf8')
         lineUtf8    = lineDecoded.encode('utf8').strip()
         if lineUtf8 == "":
-            print("a" + line + "b")
+            #print("a" + line + "b")
             continue
+        lineUtf8 = re.sub('[0-9][0-9]*', 'd', lineUtf8)
         newFileStream.write(lineUtf8 + "\n")
 
 newFileStream.close()
